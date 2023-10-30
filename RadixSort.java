d6cf622631d0756e2fcc0c09a802088b4973d4ed
Kunal Wani <krwani010120@kkwagh.edu.in>
2023-10-30 09:33:54 +0530

    Hacktoberfest

---
+++ RadixSort.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class RadixSort {
+    public static void radixSort(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+
+        int max = Arrays.stream(arr).max().getAsInt();
+        int exp = 1;
+
+        while (max / exp > 0) {
+            countingSortByDigit(arr, exp);
+            exp *= 10;
+        }
+    }
+
+    private static void countingSortByDigit(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10]; // 0 to 9 digits
+
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        radixSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
